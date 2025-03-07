faadf9ee46ceb03b886a1f9d14aa0f148c631062
Delaney Harwell <delaneyh03@gmail.com>
2025-03-07 13:09:00 -0800

    little ui for our search

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import search
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+Engine = search.SearchEngine("index.json")
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query').lower()
+    if not query:
+        return jsonify({"error": "No query provided"})
+    results = Engine.search(query)
+    results = [result[0] for result in results]
+    return jsonify({"results": results})
 
 
 if __name__ == '__main__':
-    engine = search.SearchEngine("index.json")
-    print("Welcome to the search engine!\nEnter your search query or 'exit' to exit.")
-    search.runner(engine)
-    print("Thank you for searching!")
-    search.write_report(engine)
+    app.run(host="0.0.0.0", port=5003,debug=True)
