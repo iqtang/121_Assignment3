4de76d38a4e39dc765ebafc438a18e813afddaf6
Delaney Harwell <delaneyh03@gmail.com>
2025-03-09 22:20:13 -0700

    fixed ui

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import search
+from search import *
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ranking import *
@@ -9,18 +9,19 @@ CORS(app)
 
 @app.route('/search', methods=['GET'])
 def search():
+    with open('docID_data/docIDmap.json', "r") as f:
+        docID_map = json.load(f)
     query = request.args.get('query').lower()
     if not query:
         return jsonify({"error": "No query provided"})
     offsets = get_offsets("final_index.txt")
-    engine = search.SearchEngine("final_index.txt", offsets)
-    results = engine.search(query)
-    results = [result[0] for result in results]
+    engine = SearchEngine("final_index.txt", offsets)
+    results = engine.search(query, docID_map)
     return jsonify({"results": results})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5003,debug=True)
+    app.run(host="0.0.0.0", port=5004,debug=True)
 # import search
 # from txt_retrieval import get_offsets
 #
